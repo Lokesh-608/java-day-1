5fce61596427df10f87f6c83714d87539f018eb0
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-12-10 15:25:57 +0530

    primitive

---
+++ Primitive.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        long population = 8000000000L;  // Add L for long
+        System.out.println("Population: " + population);
+    }
+}
+#double
+public class Main {
+    public static void main(String[] args) {
+        double weight = 65.789;
+        System.out.println("Weight: " + weight);
+    }
+}
