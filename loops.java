c6a2ea9ad54755a3bb0b02ec79c6f6118559218e
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-12-15 11:10:30 +0530

    updated

--- loops.java
+++ loops.java
@@ -2,7 +2,7 @@ class Loop{
     public static void main(String args[]){
         int n = 10;
         for(int i = 0;i<=n;i++){
-            if(n%2==0){
+            if(i%2==0){
                 System.out.println("Even");
             }
             else{
