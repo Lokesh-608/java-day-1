d75e649ceb970382434584fad50ea4148ca94607
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-12-15 10:07:18 +0530

    new

---
+++ loops.java
@@ -0,0 +1,13 @@
+class Loop{
+    public static void main(String args[]){
+        int n = 10;
+        for(int i = 0;i<=n;i++){
+            if(n%2==0){
+                System.out.println("Even");
+            }
+            else{
+                System.oout.println("odd");
+            }
+        }
+    }
+}
