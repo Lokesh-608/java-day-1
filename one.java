492062fe010b4e07861c150762ef0c757da35a31
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-12-18 14:45:06 +0530

    update

---
+++ one.java
@@ -0,0 +1,17 @@
+public class one {
+public int add(){
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
+return c;
+}
+public String name(){
+String frame="raju";
+return frame;
+}
+public float salary(){
+float salary=30000.0f;
+return salary:
+}
+}
